81b1eb64f8f534ffdc9b666a0af777334105e941
Amir Sekhavati <sekhavati@users.noreply.github.com>
2019-02-12 21:18:19 +0000

    Update index.js

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ javascript: (function() {
   var style = document.createElement("style");
   style.type = "text/css";
   style.innerHTML =
-    '.data-test-id { border: 2px solid hotpink; } .data-test-id::after{ content: "\\A data-test-id: \\A " attr(data-test-id); color: hotpink; white-space: pre; }';
+    '.data-test-id-highlight { border: 2px solid hotpink; } .data-test-id-highlight::after{ content: "\\A data-test-id: \\A " attr(data-test-id); color: hotpink; white-space: pre; }';
   document.getElementsByTagName("head")[0].appendChild(style);
   document.querySelectorAll("[data-test-id]").forEach(element => {
-    element.classList.add("data-test-id");
+    element.classList.add("data-test-id-highlight");
   });
 })();

c0493a406352a1964f00648dff205c3ce8a5069d
Amir Sekhavati <amir.sekhavati@gmail.com>
2019-02-17 12:32:21 +0000

    toggle highlighting on/off

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ javascript: (function() {
     '.data-test-id-highlight { border: 2px solid hotpink; } .data-test-id-highlight::after{ content: "\\A data-test-id: \\A " attr(data-test-id); color: hotpink; white-space: pre; }';
   document.getElementsByTagName("head")[0].appendChild(style);
   document.querySelectorAll("[data-test-id]").forEach(element => {
-    element.classList.add("data-test-id-highlight");
+    element.classList.toggle("data-test-id-highlight");
   });
 })();
